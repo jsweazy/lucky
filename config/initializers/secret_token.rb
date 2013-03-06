dda33a1bc1f8c05f78894c7ea1e3f0b6b99634a0
Douglas Linley Ii <doug@Douglass-MacBook-Pro.local>
2013-03-05 22:54:39 -0500

    Initial install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lucky::Application.config.secret_token = 'b3234bbb106b9f75ae8b27290f3bf6090cc51e370b6cf1749c85d1f7e5b1eb2e70034f0cbcc1be62fda1dde53145078b9c865b3ae72db14254d4184c1285edac'
